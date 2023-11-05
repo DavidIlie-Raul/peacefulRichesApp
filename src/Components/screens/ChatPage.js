4c014fe3e6ea12ff1b7fea669959f87cb4f282b3
David Raul <145023308+DavidIlie-Raul@users.noreply.github.com>
2023-11-05 23:29:57 +0100

    Added image processing for initial fetching of messages as well as for new messages, Fixed the avatar and username not being fetched for newly created messages. reverted back from slack style messages to normal ones DM Styled

--- src/Components/screens/ChatPage.js
+++ src/Components/screens/ChatPage.js
@@ -53,12 +53,6 @@ const ChatPage = () => {
             .collection("users")
             .getOne(item.sender);
           const avatarImage = userThatSentMessage.avatar;
-          console.log(
-            "Avatar of user:",
-            userThatSentMessage.username,
-            "Avatar:",
-            avatarImage
-          );
           const avatarURL = `${dbUrl}/api/files/${userThatSentMessage.collectionId}/${userThatSentMessage.id}/${avatarImage}`;
           function getAvatarImage() {
             let avatarToReturn;
@@ -73,6 +67,19 @@ const ChatPage = () => {
             }
             return avatarToReturn;
           }
+          function getMessageImage() {
+            const imageAttachment = item.attachments[0];
+            const imageURL = `${dbUrl}/api/files/${item.collectionId}/${item.id}/${item.attachments[0]}`;
+            if (
+              imageAttachment !== "" &&
+              imageAttachment !== null &&
+              imageAttachment !== undefined
+            ) {
+              return imageURL;
+            } else {
+              return undefined;
+            }
+          }
           const formattedChatMessage = {
             _id: item.id,
             text: item.content,
@@ -82,6 +89,7 @@ const ChatPage = () => {
               name: userThatSentMessage.username, // Modify this accordingly
               avatar: getAvatarImage(),
             },
+            image: getMessageImage(),
           };
           formattedMessages.push(formattedChatMessage);
         }
@@ -97,32 +105,69 @@ const ChatPage = () => {
 
   useEffect(() => {
     // Set up real-time updates
-    const unsubscribe = pb.collection("messages").subscribe("*", (data) => {
-      if (data.action === "create") {
-        // Handle new message
-        const newMessage = {
-          _id: data.record.id,
-          text: data.record.content,
-          createdAt: new Date(data.record.created),
-          user: {
-            _id: data.record.sender,
-            name: data.record.senderName, // Modify this accordingly
-          },
-        };
-        setMessages((previousMessages) =>
-          GiftedChat.append(previousMessages, [newMessage])
-        );
-      } else if (data.action === "delete") {
-        // Handle message deletion
-        const messageIdToDelete = data.record.id;
-        setMessages((previousMessages) =>
-          previousMessages.filter(
-            (message) => message._id !== messageIdToDelete
-          )
-        );
-      }
-    });
-
+    async function subToRealtimeMessages() {
+      const unsubscribe = pb
+        .collection("messages")
+        .subscribe("*", async (data) => {
+          if (data.action === "create") {
+            // Handle new message
+            const userThatSentMessage = await pb
+              .collection("users")
+              .getOne(data.record.sender);
+            const avatarImage = userThatSentMessage.avatar;
+            const avatarURL = `${dbUrl}/api/files/${userThatSentMessage.collectionId}/${userThatSentMessage.id}/${avatarImage}`;
+            function getNewAvatarImage() {
+              let avatarToReturn;
+              if (
+                avatarImage !== undefined &&
+                avatarImage !== null &&
+                avatarImage !== ""
+              ) {
+                avatarToReturn = avatarURL;
+              } else {
+                avatarToReturn = undefined;
+              }
+              return avatarToReturn;
+            }
+            function getNewMessageImage() {
+              const imageAttachment = data.record.attachments[0];
+              const imageURL = `${dbUrl}/api/files/${data.record.collectionId}/${data.record.id}/${data.record.attachments[0]}`;
+              if (
+                imageAttachment !== "" &&
+                imageAttachment !== null &&
+                imageAttachment !== undefined
+              ) {
+                return imageURL;
+              } else {
+                return undefined;
+              }
+            }
+            const newMessage = {
+              _id: data.record.id,
+              text: data.record.content,
+              createdAt: new Date(data.record.created),
+              user: {
+                _id: data.record.sender,
+                name: userThatSentMessage.username, // Modify this accordingly
+                avatar: getNewAvatarImage(),
+              },
+              image: getNewMessageImage(),
+            };
+            setMessages((previousMessages) =>
+              GiftedChat.append(previousMessages, [newMessage])
+            );
+          } else if (data.action === "delete") {
+            // Handle message deletion
+            const messageIdToDelete = data.record.id;
+            setMessages((previousMessages) =>
+              previousMessages.filter(
+                (message) => message._id !== messageIdToDelete
+              )
+            );
+          }
+        });
+    }
+    subToRealtimeMessages();
     return () => {
       // Unsubscribe when the component unmounts
       pb.collection("messages").unsubscribe("*");
@@ -158,7 +203,6 @@ const ChatPage = () => {
         messages={messages}
         renderUsernameOnMessage={true}
         showAvatarForEveryMessage={true}
-        renderMessage={renderMessage}
         onSend={(newMessage) => handleSend(newMessage)}
         user={{
           _id: user.id, // Use the ID of the current user
@@ -172,6 +216,7 @@ const ChatPage = () => {
                 right: {
                   backgroundColor: "lightblue", // Set the left bubble background color
                 },
+                left: { backgroundColor: "#D5D5D5" },
               }}
             />
           );
